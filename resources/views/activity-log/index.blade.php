457561375e7b809a4320c13ba3d1154186b4b011
x-eyn <zain.ahmed@g.bracu.ac.bd>
2023-08-20 01:32:54 +0600

    fix

--- resources/views/activity-log/index.blade.php
+++ resources/views/activity-log/index.blade.php
@@ -8,27 +8,31 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-8">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>User ID</th>
-                            <th>Action</th>
-                            <th>Description</th>
-                            <th>Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($logs as $log)
+                <div class="table-responsive">
+                    <table class="table table-hover border border-gray-600 rounded-lg">
+                        <thead class="bg-gray-800 text-white">
                             <tr>
-                                <td>{{ $log->user_id }}</td>
-                                <td>{{ $log->action }}</td>
-                                <td>{{ $log->description }}</td>
-                                <td>{{ $log->created_at->format('Y-m-d H:i:s') }}</td>
+                                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">User ID</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Action</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Description</th>
+                                <th class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">Date</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                {{ $logs->links() }} <!-- Pagination links -->
+                        </thead>
+                        <tbody class="divide-y divide-gray-600">
+                            @foreach($logs as $log)
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ optional($log->user)->name ?? 'N/A' }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $log->action }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $log->description }}</td>
+                                    <td class="px-6 py-4 whitespace-nowrap">{{ $log->created_at->format('Y-m-d H:i:s') }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="mt-4">
+                    {{ $logs->links() }} <!-- Pagination links -->
+                </div>
             </div>
         </div>
     </div>
